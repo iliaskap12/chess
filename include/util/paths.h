3efda0a9e53969ad92357e5b0eef3ba3b74eb801
iliaskap12 <iliaskapareliotis@gmail.com>
2022-01-04 15:29:48 +0200

    Add path utilities

---
+++ include/util/paths.h
@@ -0,0 +1,12 @@
+#ifndef CHESS_PATHS_H
+#define CHESS_PATHS_H
+
+#include <string>
+#include <filesystem>
+#include <iostream>
+
+std::string getProjectRootPath();
+std::string getImagesPath();
+std::string getSoundsPath();
+
+#endif//CHESS_PATHS_H
---
+++ src/util/paths.cpp
@@ -0,0 +1,15 @@
+#include <util/paths.h>
+
+std::string getProjectRootPath() {
+  return std::filesystem::current_path().make_preferred().parent_path().parent_path();
+}
+
+std::string getImagesPath() {
+  std::string root = getProjectRootPath();
+  return std::filesystem::path(root + "/assets/images/", std::filesystem::path::native_format);
+}
+
+std::string getSoundsPath() {
+  std::string root = getProjectRootPath();
+  return std::filesystem::path(root + "/assets/sounds/", std::filesystem::path::native_format);
+}
