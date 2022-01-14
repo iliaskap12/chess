e91bbeeb8f97d627ada6caaeeb1560ed3b0b581f
iliaskap12 <iliaskapareliotis@gmail.com>
2022-01-14 17:30:05 +0200

    Add vcpkg as a git submodule, add sgg dependencies in vcpkg.json, add cmake presets, add sgg with fetch content
    Took 2 hours 3 minutes

--- src/util/paths.cpp
+++ src/util/paths.cpp
@@ -1,20 +1,23 @@
 #include <util/paths.h>
 
 std::string paths::getProjectRootPath() {
-  return std::filesystem::current_path().make_preferred().parent_path().parent_path();
+  return std::filesystem::current_path().make_preferred().parent_path().parent_path().parent_path().parent_path();
 }
 
 std::string paths::getImagesPath() {
-  std::string root { paths::getProjectRootPath() };
+  std::string root{paths::getProjectRootPath()};
+  std::cout << root << std::endl;
   return std::filesystem::path(root + "/assets/images/", std::filesystem::path::native_format);
 }
 
 std::string paths::getSoundsPath() {
-  std::string root { paths::getProjectRootPath() };
+  std::string root{paths::getProjectRootPath()};
+  std::cout << root << std::endl;
   return std::filesystem::path(root + "/assets/sounds/", std::filesystem::path::native_format);
 }
 
 std::string paths::getFontsPath() {
-  std::string root { paths::getProjectRootPath() };
+  std::string root{paths::getProjectRootPath()};
+  std::cout << root << std::endl;
   return std::filesystem::path(root + "/assets/fonts/", std::filesystem::path::native_format);
 }
