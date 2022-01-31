ca9e40fbe51778cf1c67d099e3bc7d59b5bb152e
iliaskap12 <62548937+iliaskap12@users.noreply.github.com>
2022-01-31 17:38:42 +0000

    Deploying to gh-pages from @ iliaskap12/chess@2d6f48bd9f568ac6dae36f2855b5144837462b61 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['bishop_0',['Bishop',['../de/dfb/classBishop.html#a618beba64f4c10cbd02fe349b575e2e7',1,'Bishop']]],
-  ['block_1',['block',['../de/dff/classPawn.html#a562f28302eb1503d8fb061ec7fe937e1',1,'Pawn']]],
-  ['blockedby_2',['blockedBy',['../de/dff/classPawn.html#a1e2f863c16fbc247b5a8f8e2891a645b',1,'Pawn']]]
+  ['banenpassanton_0',['banEnPassantOn',['../d9/dbe/classSoldier.html#ae3cdc9bd119fa3006b94c4f8ea27bee3',1,'Soldier']]],
+  ['bishop_1',['Bishop',['../de/dfb/classBishop.html#a618beba64f4c10cbd02fe349b575e2e7',1,'Bishop']]],
+  ['block_2',['block',['../de/dff/classPawn.html#a562f28302eb1503d8fb061ec7fe937e1',1,'Pawn']]],
+  ['blockedby_3',['blockedBy',['../de/dff/classPawn.html#a1e2f863c16fbc247b5a8f8e2891a645b',1,'Pawn']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,13 +1,14 @@
 var searchData=
 [
-  ['blackdangerreferencecount_5f_0',['blackDangerReferenceCount_',['../d3/d72/classSquare.html#a2bd6cda99348c0def34fdb6133085db8',1,'Square']]],
-  ['blackking_1',['blackKing',['../d7/d22/classCheckboard.html#a4d8615b42771cc15f7f6db10fad2b5d6',1,'Checkboard']]],
-  ['blackkingid_2',['blackKingId',['../d7/d22/classCheckboard.html#ae16881b48fca2f5598640cb273d66b2c',1,'Checkboard']]],
-  ['blackkingthreat_3',['blackKingThreat',['../d7/d22/classCheckboard.html#a4b2f8638f40008fd5a31b190fe86a881',1,'Checkboard']]],
-  ['blacksturnmessage_4',['blacksTurnMessage',['../de/d14/classTurnIndicator.html#aec1dbc6e8f86460eb83fd60d75394ae6',1,'TurnIndicator']]],
-  ['blocked_5',['blocked',['../de/dff/classPawn.html#a10a904cc5cfdcb600830ab128c417a19',1,'Pawn']]],
-  ['blockingpawn_6',['blockingPawn',['../de/dff/classPawn.html#ae1ce8e9e66831a7cfbfd532f76ef34aa',1,'Pawn']]],
-  ['brush_5f_7',['brush_',['../de/d00/classRectangle.html#a97679fabd0f231f6e21679b21f1ea9e6',1,'Rectangle']]],
-  ['brushtype_8',['brushType',['../de/d00/classRectangle.html#a7bc8e6e8c2cc66616e0895881749c12b',1,'Rectangle']]],
-  ['button_9',['button',['../dd/d2c/classMenuOption.html#ab509e9a62e1133125955c455f7b8553d',1,'MenuOption']]]
+  ['bannedenpassantsquare_0',['bannedEnPassantSquare',['../d9/dbe/classSoldier.html#a222086b8c4fd3efaeac12ba40fb48ad4',1,'Soldier']]],
+  ['blackdangerreferencecount_5f_1',['blackDangerReferenceCount_',['../d3/d72/classSquare.html#a2bd6cda99348c0def34fdb6133085db8',1,'Square']]],
+  ['blackking_2',['blackKing',['../d7/d22/classCheckboard.html#a4d8615b42771cc15f7f6db10fad2b5d6',1,'Checkboard']]],
+  ['blackkingid_3',['blackKingId',['../d7/d22/classCheckboard.html#ae16881b48fca2f5598640cb273d66b2c',1,'Checkboard']]],
+  ['blackkingthreat_4',['blackKingThreat',['../d7/d22/classCheckboard.html#a4b2f8638f40008fd5a31b190fe86a881',1,'Checkboard']]],
+  ['blacksturnmessage_5',['blacksTurnMessage',['../de/d14/classTurnIndicator.html#aec1dbc6e8f86460eb83fd60d75394ae6',1,'TurnIndicator']]],
+  ['blocked_6',['blocked',['../de/dff/classPawn.html#a10a904cc5cfdcb600830ab128c417a19',1,'Pawn']]],
+  ['blockingpawn_7',['blockingPawn',['../de/dff/classPawn.html#ae1ce8e9e66831a7cfbfd532f76ef34aa',1,'Pawn']]],
+  ['brush_5f_8',['brush_',['../de/d00/classRectangle.html#a97679fabd0f231f6e21679b21f1ea9e6',1,'Rectangle']]],
+  ['brushtype_9',['brushType',['../de/d00/classRectangle.html#a7bc8e6e8c2cc66616e0895881749c12b',1,'Rectangle']]],
+  ['button_10',['button',['../dd/d2c/classMenuOption.html#ab509e9a62e1133125955c455f7b8553d',1,'MenuOption']]]
 ];
